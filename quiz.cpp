7044cf1290be42a6698fa1ea3c2c2b8596c011e9
GustavoLiu <gustavo_santostr@hotmail.com>
2019-02-12 20:03:59 -0200

    Quiz

---
+++ quiz.cpp
@@ -0,0 +1,115 @@
+#include<iostream>
+
+
+using namespace std;
+
+int main ()
+{
+int acertos;
+int resposta;
+
+
+//////////pergunta 1/////////////
+cout << "Quanto é 1+15?\n
+1. 15\n
+2. 16\n
+3. 17\n
+4. 18\n
+Digite o número correspondente a alternativa de sua resposta: ";
+cin >> resposta;
+if (resposta1 == 2){
+  acerto = acerto+1;
+}else{
+  acerto = acerto;
+}
+///////////////////////////////////
+
+////pergunta 2//////
+cout << "Quanto é 55*2?\n
+1. 541\n
+2. 50\n
+3. 110\n
+4. 100\n
+Digite o número correspondente a alternativa de sua resposta: ";
+cin >> resposta;
+if (resposta1 == 3){
+  acerto = acerto+1;
+}else{
+  acerto = acerto;
+}
+
+/////pergunta 3//////
+cout << "Qual era a cor do cavalo Branco de Napoleão?\n
+1. Marrom\n
+2. Branco\n
+3. Preto\n
+4. Cinza\n
+Digite o número correspondente a alternativa de sua resposta: ";
+cin >> resposta;
+if (resposta1 == 1){
+  acerto = acerto+1;
+}else{
+  acerto = acerto;
+}
+
+///////pergunta 4/////
+cout << "Quanto é 1+15?\n
+1. 15\n
+2. 16\n
+3. 17\n
+4. 18\n
+Digite o número correspondente a alternativa de sua resposta: ";
+cin >> resposta1;
+if (resposta1 == 2){
+  acerto = acerto+1;
+}else{
+  acerto = acerto;
+}
+
+
+//////////pergunta 5/////////
+cout << "Qual das alternativas tem \"dois zeros, dois zero\"?\n
+1. 2000\n
+2. 0020\n
+3. 2020\n
+4. 2022\n
+Digite o número correspondente a alternativa de sua resposta: ";
+cin >> resposta1;
+if (resposta1 == 2){
+  acerto = acerto+1;
+}else{
+  acerto = acerto;
+}
+
+////// pergunta 6 /////
+cout << "Quem descobriu o Brasil foi Pedro Álvares...\n
+1. Cobra\n
+2. Souza\n
+3. Cabra\n
+4. Cabral\n
+Digite o número correspondente a alternativa de sua resposta: ";
+cin >> resposta1;
+if (resposta1 == 4){
+  acerto = acerto+1;
+}else{
+  acerto = acerto;
+}
+
+///////pergunta 7////////
+cout << "?\n
+1. 15\n
+2. 16\n
+3. 17\n
+4. 18\n
+Digite o número correspondente a alternativa de sua resposta: ";
+cin >> resposta1;
+if (resposta1 == 2){
+  acerto = acerto+1;
+}else{
+  acerto = acerto;
+}
+
+
+
+
+}
